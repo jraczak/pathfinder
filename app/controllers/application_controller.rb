ecb47ef361768852ed833c1e0cdb68798518f9bb
Justin Raczak <jraczak@gmail.com>
2013-03-15 14:32:25 -0700

    Add facebook_sign_in_and_redirect method to Application controller to attempt to fix no_method_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def facebook_sign_in_and_redirect(resource_or_scope, *args)
+    options  = args.extract_options!
+    scope    = Devise::Mapping.find_scope!(resource_or_scope)
+    resource = args.last || resource_or_scope
+    sign_in(scope, resource, options)
+    redirect_to user_confirmation_path
+  end
+  
 end
