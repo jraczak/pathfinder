25b20417a8ac84b44276274890f497ec2a8bc90f
Justin Raczak <jraczak@gmail.com>
2013-03-15 14:40:03 -0700

    Remove facebook_sign_in_and_redirect from Registrations Controller, save in Application Controller, change redirect_to to confirmation_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     scope    = Devise::Mapping.find_scope!(resource_or_scope)
     resource = args.last || resource_or_scope
     sign_in(scope, resource, options)
-    redirect_to user_confirmation_path
+    redirect_to confirmation_url(resource, :confirmation_token => resource.confirmation_token)
   end
   
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def facebook_sign_in_and_redirect(resource_or_scope, *args)
-    options  = args.extract_options!
-    scope    = Devise::Mapping.find_scope!(resource_or_scope)
-    resource = args.last || resource_or_scope
-    sign_in(scope, resource, options)
-    redirect_to user_confirmation_path
-  end
+
 
 end
